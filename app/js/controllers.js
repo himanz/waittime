049fba5f6a2a4a2f926461f92e60586bc9cfd65c
Jonathan Chang <jonoschang7@gmail.com>
2014-09-13 16:11:16 -0400

    add firebase dependency to waitlist controller

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,13 +6,15 @@ angular.module('myApp.controllers', [])
   .controller('LandingPageController', [function() {
 
   }])
-  .controller('WaitlistController', ['$scope', function($scope) {
-    $scope.parties = [];
+  .controller('WaitlistController', ['$scope', '$firebase', function($scope, $firebase) {
+    var partiesRef = new Firebase('https://waitandeat-jonos.firebaseio.com/')
+    
+    $scope.parties = $firebase(partiesRef);
 
     $scope.party = {name: '', phone: '', size: ''};
 
     $scope.saveParty = function() {
-    	$scope.parties.push($scope.party);
+    	$scope.parties.$add($scope.party);
       $scope.party = {name: '', phone: '', size: ''};    	
     };
   }])
