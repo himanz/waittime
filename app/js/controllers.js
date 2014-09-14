fa3385340f5b87afc2e06af121c838d235df0ca6
Jonathan Chang <jonoschang7@gmail.com>
2014-09-14 14:12:42 -0400

    add notified feature in controller

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -13,12 +13,12 @@ angular.module('myApp.controllers', [])
     $scope.parties = $firebase(partiesRef);
 
     // Object to store data from the waitlist form.
-    $scope.newParty = {name: '', phone: '', size: '', done: false};
+    $scope.newParty = {name: '', phone: '', size: '', done: false, notified: 'No'};
 
     // Function to save a new party to the waitlist.
     $scope.saveParty = function() {
     	$scope.parties.$add($scope.newParty);
-      $scope.newParty = {name: '', phone: '', size: '', done: false};    	
+      $scope.newParty = {name: '', phone: '', size: '', done: false, notified: 'No'};    	
     };
 
     // Function to send text message to a party
@@ -31,5 +31,7 @@ angular.module('myApp.controllers', [])
         name: party.name
       };
       textMessages.$add(newTextMessage);
+      party.notified = "Yes";
+      $scope.parties.$save(party.$id);
     };
   }])
