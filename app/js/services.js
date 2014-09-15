597dbf6866417d9516bee47cf62883aab3efabed
Jonathan Chang <jonoschang7@gmail.com>
2014-09-15 14:54:35 -0400

    put firebase repetitive code into dataService

--- app/js/services.js
+++ app/js/services.js
@@ -7,9 +7,14 @@
 // In this case it is a simple value service.
 angular.module('myApp.services', [])
   .value('FIREBASE_URL', 'https://waitandeat-jonos.firebaseio.com/')
-  .factory('partyService', function($firebase, FIREBASE_URL) {
-    var partiesRef = new Firebase(FIREBASE_URL + 'parties')
-    var parties = $firebase(partiesRef);
+  .factory('dataService', function($firebase, FIREBASE_URL) {
+  	var dataRef = new Firebase(FIREBASE_URL);
+  	var fireData = $firebase(dataRef);
+
+  	return fireData;
+  })
+  .factory('partyService', function(dataService) {
+    var parties = dataService.$child('parties') 
 
     var partyServiceObject = {
       parties: parties,
@@ -20,9 +25,8 @@ angular.module('myApp.services', [])
 
     return partyServiceObject
   })
-  .factory('textMessageService', function($firebase, FIREBASE_URL, partyService) {
-  	var textMessageRef = new Firebase(FIREBASE_URL + 'textMessages');
-    var textMessages = $firebase(textMessageRef);
+  .factory('textMessageService', function(dataService, partyService) {
+    var textMessages = dataService.$child('textMessages');
 
     var textMessageServiceObject = {
       sendTextMessage: function(party) {
